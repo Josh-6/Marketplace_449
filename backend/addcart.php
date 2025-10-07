ea1fb635a49c47f7a683858a12128e47e9ed9709
tonydinh1990 <tonydinh1990@csu.fullerton.edu>
2025-10-06 23:56:18 -0700

    Add files via upload

---
+++ backend/addcart.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $id = $_POST['id'];
+  $name = $_POST['name'];
+  $price = $_POST['price'];
+
+  // Initialize cart if empty
+  if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = [];
+  }
+
+  // Check if item already exists in cart
+  if (isset($_SESSION['cart'][$id])) {
+    $_SESSION['cart'][$id]['quantity']++;
+  } else {
+    $_SESSION['cart'][$id] = [
+      'name' => $name,
+      'price' => $price,
+      'quantity' => 1
+    ];
+  }
+
+  header("Location: cart.php");
+  exit;
+}
+?>
