687bc89d7e148d7e5a465cd81d20d84b1a48966f
AlejandroMamani <alejandrox0011@csu.fullerton.edu>
2025-10-24 22:56:57 -0700

    - fixing samples - fixing clear tables

--- database/sampleData.php
+++ database/sampleData.php
@@ -71,14 +71,16 @@
         VALUES
         (1, 1, NULL, 'Laptop', 'High performance laptop', 1200.00, 'electronics,computer', 10, CURRENT_TIMESTAMP),
         (2, 2, NULL, 'Novel', 'Bestselling fiction novel', 20.00, 'books,reading,Toy and Hobbies', 50, CURRENT_TIMESTAMP),
-        (3, 1, NULL, 'Sonic Headphones', 'Noise-cancelling headphones', 150.00, 'electronics,audio', 25, CURRENT_TIMESTAMP)
+        (3, 1, NULL, 'Sonic Headphones', 'Noise-cancelling headphones', 150.00, 'electronics,audio', 25, CURRENT_TIMESTAMP),
         (4, 2, NULL, 'Barbie Doll', 'Over prized doll for collectors', 500.00, 'Toy and Hobbies,collectibles', 1, CURRENT_TIMESTAMP),
         (5, 2, NULL, 'Sinshian Apples', 'Pack of 7 fresh apples made up of tears of sinshian children', 40.00, 'Food', 100, CURRENT_TIMESTAMP),
-        (6, 1, NULL, 'Cheap Headphones', 'CHEAP Noise-cancelling headphones for cheap, cheap', 40.00, 'electronics,audio', 25, CURRENT_TIMESTAMP)
+        (6, 1, NULL, 'Cheap Headphones', 'CHEAP Noise-cancelling headphones for cheap, cheap', 40.00, 'electronics,audio', 25, CURRENT_TIMESTAMP),
         (7, 2, NULL, 'Moms ring', 'Ring for moms from my mom', 2000.00, 'Fashion and Apparel', 1, CURRENT_TIMESTAMP),
-        (8, 1, NULL, 'Stools', 'Noise-cancelling stool for your home', 150.00, 'Furniture', 25, CURRENT_TIMESTAMP)
+        (8, 1, NULL, 'Stools', 'Noise-cancelling stool for your home', 150.00, 'Furniture', 25, CURRENT_TIMESTAMP),
         (9, 2, NULL, 'Avatar the love machine', 'Bestselling fiction novel about love', 25.00, 'Fashion and Apparel', 50, CURRENT_TIMESTAMP),
-        (10, 1, NULL, 'Mr love lipstick', 'Noise-cancelling headphones', 150.00, 'Beauty and Personal Care', 25, CURRENT_TIMESTAMP)
+        (10, 1, NULL, 'Mr love lipstick', 'Noise-cancelling headphones', 150.00, 'Beauty and Personal Care', 25, CURRENT_TIMESTAMP),
+        (11, 2, NULL, 'Wooden table', 'Solid wood dining table', 300.00, 'Furniture', 5, CURRENT_TIMESTAMP),
+        (12, 1, NULL, 'Organic Bananas', 'Fresh organic bananas', 30.00, 'Food', 80, CURRENT_TIMESTAMP)
         ;"
         ;
 
